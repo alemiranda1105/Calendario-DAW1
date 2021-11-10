cb5b462c7e5031b3f298144e1a8625920ae24723
EmilioMartel <emiliomtg@gmail.com>
2021-11-10 10:26:10 +0100

    getGroups y getGroupById(id) implementadas correctamente

--- data/db.service.js
+++ data/db.service.js
@@ -19,12 +19,31 @@ export async function getData(ruta) {
     }
 }
 
-//@param: jwt es el token unico de cada user
-export async function getUser(jwt) {
+export async function getUsers() {
     let result;
     try{
         result = await $.ajax({
-        url: `${URL}users/${jwt}`,
+        url: `${URL}users`,
+        type: 'GET',
+        success: function(res) {
+            result = res;
+        },
+        error: function() {
+            console.error("No es posible completar la operación");
+        }
+    });
+    return result;
+    }catch(error){
+        console.error(error);
+    }
+}
+
+//@param id: token unico de cada user
+export async function getUserById(id) {
+    let result;
+    try{
+        result = await $.ajax({
+        url: `${URL}users/${id}`,
         type: 'GET',
         success: function(res) {
             result = res;
@@ -61,8 +80,44 @@ export async function getCurrentUser() {
     }else {
         // EN CASO DE QUE BORREN LA KEY, REDIRIGIR AL LOGIN
     }
-   
 }
 
+export async function getGroups() {
+    let result;
+    try{
+        result = await $.ajax({
+        url: `${URL}groups`,
+        type: 'GET',
+        success: function(res) {
+            result = res;
+        },
+        error: function() {
+            console.error("No es posible completar la operación");
+        }
+    });
+    return result;
+    }catch(error){
+        console.error(error);
+    }
+}
 
+//@param id: identificador unico de cada grupo
+export async function getGroupById(id) {
+    let result;
+    try{
+        result = await $.ajax({
+        url: `${URL}users/${id}`,
+        type: 'GET',
+        success: function(res) {
+            result = res;
+        },
+        error: function() {
+            console.error("No es posible completar la operación");
+        }
+    });
+    return result;
+    }catch(error){
+        console.error(error);
+    }
+}
 
