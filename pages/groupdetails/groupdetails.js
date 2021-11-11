0edf6da151c1e65315dce613775b9c48c906191c
Raul F. Sanchez Falcon <raulsf10@gmail.com>
2021-11-11 20:57:00 +0000

    pagina de agregar amigos y cambios js groupdetails

--- pages/groupdetails/groupdetails.js
+++ pages/groupdetails/groupdetails.js
@@ -2,11 +2,13 @@ let groupId = localStorage.getItem('groupId');
 localStorage.removeItem('groupInfo');
 let list = document.getElementById("listUsers");
 let groupName = document.getElementById("groupName");
-
+let pencil = document.getElementById("pencil");
+let nameAux;
 getCurrentUser().then((user) =>{
     getGroupById(groupId).then((group) => {
         //No se escribe el contenido en el input pero al inspeccionar si tiene el contenido correcto
-        groupName.innerHTML = group.name;
+        groupName.value = group.name;
+        nameAux = groupName.value ;
         for(let i = 0; i < group.users.length; i++){
             if(group.users[i] == user.id){
                 continue;
@@ -22,3 +24,19 @@ getCurrentUser().then((user) =>{
         };
     });
 });
+
+pencil.addEventListener('click', () => cambiaNombre());
+
+function cambiaNombre(){
+    if(groupName.disabled){
+        groupName.removeAttribute("disabled");
+    } else{
+        groupName.setAttribute("disabled", true);
+        console.log(groupName.value, '-', nameAux);
+        if(groupName.value !== nameAux){
+            //Escribir nuevo nombre en la base de datos
+            console.log('El nombre del grupo ha cambiado');
+            nameAux = groupName.value;
+        }
+    }
+}
