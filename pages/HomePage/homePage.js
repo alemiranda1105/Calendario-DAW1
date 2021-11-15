34782cccb223a30429118fb0d3e61e3f64d87724
Emilio <emiliomtg@gmail.com>
2021-11-15 19:25:53 +0000

    cambiando diseño a tono más alegre/llamativo

--- pages/HomePage/homePage.js
+++ pages/HomePage/homePage.js
@@ -1,6 +1,6 @@
 
 
-let monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre', 'Noviembre', 'Deciembre'];
+let monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre', 'Noviembre', 'Diciembre'];
 
 let currentDate = new Date();
 let currentDay = currentDate.getDate();
