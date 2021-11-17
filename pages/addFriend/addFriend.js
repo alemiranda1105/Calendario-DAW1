f6f57f2e6c30422fb4cf009e44ebe86c2a778e42
Raul F. Sanchez Falcon <raulsf10@gmail.com>
2021-11-17 13:13:01 +0000

    js addfriend

--- pages/addFriend/addFriend.js
+++ pages/addFriend/addFriend.js
@@ -1,5 +1,40 @@
-let search = document.getElementById("search");
+let search = document.getElementById("search");                
+let lista = [];
+let user;
+let info = document.getElementById("info");
+getCurrentUser().then(data =>{
+    if(data === undefined){
+        window.location.replace("http://localhost:5500/pages/inicio/inicio.html");
+    } else{
+        user = data
+    }
+}).then(()=>{
+    getUsers().then(users =>{
+        let friends = user.friends;
+        users.forEach((actual) =>{
+            if(friends.includes(''+actual.id)==false && actual.id != user.id){
+                lista.push(actual.username);
+            }
+        });
+    });
+});
 
-const filtrar = () =>{
-    
-};
+function enviar(){
+    console.log(search.value);
+    getCurrentUser().then((user)=>{
+        getUserByUsername(search.value).then((userEnviar)=>{
+            console.log(userEnviar[0].friendRequests);
+            if(!userEnviar[0].friendRequests.includes(''+user.id)){
+                userEnviar[0].friendRequests.push("" + user.id);
+                info.innerHTML='Solicitud enviada con éxito';
+            } else{
+                info.innerHTML='Ya has enviado una solicitud al usuario anteriormente';
+            }
+            console.log(userEnviar[0].friendRequests);
+        });
+    });
+}
+
+$(search).autocomplete({
+    source: lista
+});
