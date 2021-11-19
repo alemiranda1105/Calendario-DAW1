005e2fde4c799d77b8ce65034379ec822dca0e1e
Emilio <emiliomtg@gmail.com>
2021-11-19 17:45:30 +0000

    eventos individuales agregados al calendario, vista actual, previa y post

--- pages/HomePage/homePage.js
+++ pages/HomePage/homePage.js
@@ -22,46 +22,52 @@ nextMonthDOM.addEventListener('click', ()=>nextMonth());
 let aux = new Date();
 var auxCurrentDate = {
     day: aux.getDate(),
-    month: monthNumber,
+    month: aux.getMonth(),
     year: aux.getFullYear()
 }
 
 var acd;
-var monthYear = `${auxCurrentDate.month+1}-${auxCurrentDate.month}`;
+var monthYear = `${auxCurrentDate.month+1}-${auxCurrentDate.year}`;
 /** CALENDARIO */
 function writeMonth(month) {
 
     //dias del mes pasado visibles en el mes actual
     for(let i = startDay(); i>0;i--){
+        if(monthNumber === 0){
+            acd = `${getTotalDays(monthNumber-1)-(i-1)}-12-${auxCurrentDate.year-1}`;
+        }else{
+            acd = `${getTotalDays(monthNumber-1)-(i-1)}-${monthNumber}-${auxCurrentDate.year}`;
+        }
+        
         dates.innerHTML += 
-        ` <div id="divLastDays${getTotalDays(monthNumber-1)-(i-1)}" class="calendar__item calendar__last-days">
-            <div id="lastDay${getTotalDays(monthNumber-1)-(i-1)}" class="d-flex justify-content-center">
+        ` <div id="divLastDay${acd}" class="calendar__item calendar__last-days">
+            <div id="day${acd}" class="d-flex justify-content-center">
                ${getTotalDays(monthNumber-1)-(i-1)}
             </div>
             
         </div>`;
+
     }
     for(let i=1; i<=getTotalDays(month); i++){
+        acd = `${i}-${auxCurrentDate.month+1}-${auxCurrentDate.year}`;
         if(i===currentDay && monthNumber === aux.getMonth() && currentYear === aux.getFullYear()) {
             dates.innerHTML += `
-            <div id="divCurrentDay${i}" class="calendar__today divTxtDayEvent">
+            <div id="div${auxCurrentDate.day}-${auxCurrentDate.month+1}-${auxCurrentDate.year}" class="calendar__today divTxtDayEvent">
                 <div id="day${auxCurrentDate.day}-${auxCurrentDate.month+1}-${auxCurrentDate.year}" class="d-flex justify-content-center">
                     ${i}
                 </div>
 
-                <div id="eventDay${i}" class="txtDayEvent bg-grey">
+                <div id="eventDay${auxCurrentDate.day}-${auxCurrentDate.month+1}-${auxCurrentDate.year}" class="txtDayEvent bg-grey">
                     numero: ${i} 
                 </div>
             </div>`;
         }else{
             acd = `${i}-${auxCurrentDate.month+1}-${auxCurrentDate.year}`;
             dates.innerHTML += `
-            <div id="divDay${i}" class="divTxtDayEvent calendar__item">
+            <div id="divDay${acd}" class="divTxtDayEvent calendar__item">
                 <div id="day${acd}">
                     ${i}
                 </div>
-
-               
             </div>`;
         }
     }
@@ -69,12 +75,72 @@ function writeMonth(month) {
     //dias del mes proximo en el mes actual
     let j = 1;
     for(let i = lastDay(); i < 6;i++){
-        dates.innerHTML += 
-        ` <div id="postDays${i}" class="calendar__item calendar__last-days">
-            ${j}
+        acd = `${j}-${auxCurrentDate.month+2}-${auxCurrentDate.year}`;
+        dates.innerHTML += `
+        <div id="divPostDay${acd}" class="calendar__item calendar__last-days">
+            <div id="day${acd}">
+                ${i}
+            </div>
         </div>`;
         j++;
     }
+
+    //UNA VEZ CARGADO EL CALENDARIO, CARGAMOS LOS EVENTOS
+    mostrarEventoListado().then(({fechas,eventos}) => {
+        console.log("fecha", fechas);
+        console.log("eventos", eventos);
+    
+        $('#dates').find('div').each(function(){
+            var divIds = $(this).attr('id');
+
+            //MES ANTERIOR VISIBLE
+            if(divIds.startsWith("divLastDay")){
+                var idNumerico= ($(this)[0].id).replace("divLastDay","");
+                for(var i = 0; i < eventos.length; i++){
+                    if(idNumerico === eventos[i].date){
+                        $($(this)).append(
+                            `<div id="lastEventDay${idNumerico}" class="txtDayEvent bg-blue">
+                                ${eventos[i].name}
+                            </div>`);
+                    }
+                }   
+            }
+    
+            //MES ACTUAL
+            if(divIds.startsWith("divDay")){
+                var idNumerico= ($(this)[0].id).replace("divDay","");
+                for(var i = 0; i < eventos.length; i++){
+                    if(idNumerico === eventos[i].date){
+                        $($(this)).append(
+                            `<div id="eventDay${idNumerico}" class="txtDayEvent bg-blue">
+                                ${eventos[i].name}
+                            </div>`);
+                    }
+                }
+    
+                //CLICK EN EL EVENTO TE LLEVA A EDITAR ESE EVENTO
+                var id = `eventDay${idNumerico}`;
+                $(`#${id}`).click((e)=>{
+                    alert(`${id}`);
+    
+                    e.stopPropagation();
+                })
+            }
+    
+            //MES PROXIMO VISIBLE
+            if(divIds.startsWith("divPostDay")){
+                var idNumerico= ($(this)[0].id).replace("divPostDay","");
+                for(var i = 0; i < eventos.length; i++){
+                    if(idNumerico === eventos[i].date){
+                        $($(this)).append(
+                            `<div id="postEventDay${idNumerico}-${i}" class="txtDayEvent bg-blue">
+                                ${eventos[i].name}
+                            </div>`);
+                    }
+                }   
+            }
+        });
+    });
 }
 
 const getTotalDays = month => {
@@ -107,37 +173,26 @@ const lastDay = () => {
 
 const lastMonth = () => {
    
-
     if(monthNumber !== 0){
         monthNumber--;
         auxCurrentDate.month--;
-        console.log("monthNumer", monthNumber);
-        console.log("auxCurrentDate", auxCurrentDate.month);
     }else{
         monthNumber = 11;
         currentYear--;
         auxCurrentDate.year--;
-        console.log("monthNumer", monthNumber);
-        console.log("auxCurrentDate", auxCurrentDate.month);
     }
-
     setNewDate();
 }
 
 const nextMonth = () => {
-    
     if(monthNumber !== 11){
         monthNumber++;
         auxCurrentDate.month++;
-        console.log("monthNumer", monthNumber+1);
-        console.log("auxCurrentDate", auxCurrentDate.month+1);
     }else{
         monthNumber = 0;
         currentYear++;
         auxCurrentDate.month = 0;
         auxCurrentDate.year++;
-        console.log("monthNumer", monthNumber+1);
-        console.log("auxCurrentDate", auxCurrentDate.month+1);
     }
     setNewDate();
 }
@@ -182,53 +237,27 @@ getCurrentUser().then((user) =>{
     }
 });
 
-var fecha = [];
+var auxFecha = [];
 async function mostrarEventoListado(){
-    var a = getCurrentUser().then((user) =>{
+    var resp = getCurrentUser().then((user) =>{
         var listado = [];
-        var fechaSalida ={}, eventoSalida=[];
+        var fechas ={}, eventos=[];
         for(let i = 0; i < user.events.length; i++) {
             listado[i] = user.events[i];
             var aux = listado[i].date;           
             var date = aux.split("-");
             
-            fecha[i] = {
+            auxFecha[i] = {
                 day: parseInt(date[0]),
                 month: parseInt(date[1]),
                 year: parseInt(date[2])
             }
-            fechaSalida[i] = fecha[i];
-            eventoSalida[i] = user.events[i];
+            fechas[i] = auxFecha[i];
+            eventos[i] = user.events[i];
         }
-        return {fechaSalida,eventoSalida};
+        return {fechas,eventos};
     });
-    return a;
+
+    return resp;
 }
 
-mostrarEventoListado().then((fecha, eventos) => {
-    $('#dates').find('div').each(function(){
-        var divIds = $(this).attr('id');
-
-        if(divIds.startsWith("divDay")){
-            var idNumerico= ($(this)[0].id).replace("divDay","");
-            ;
-            $($(this)).append(
-            `<div id="eventDay${idNumerico}-${monthYear}" class="txtDayEvent bg-blue">
-                numero: ${idNumerico}
-            </div>`);
-        }
-        var aux = document.getElementById(divIds);
-        aux.addEventListener('click', (e) => {
-            
-            
-            alert(divIds);
-            e.stopPropagation();
-        })
-
-        if(divIds.startsWith("day")){
-            $(this).find("div").each(()=>{
-
-            })
-        }
-    });
-});
