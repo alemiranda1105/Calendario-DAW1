3016645472271b288163b98cfb8a73395598dc87
Raul F. Sanchez Falcon <raulsf10@gmail.com>
2021-11-22 18:21:37 +0000

    Responsive pagina solicitudes de amistad y goback de friendProfile

--- pages/friendProfilePage/friendProfilePage.js
+++ pages/friendProfilePage/friendProfilePage.js
@@ -1,30 +1,54 @@
 let id = localStorage.getItem('id');
-localStorage.removeItem('id');
-let url = localStorage.getItem('url');
-localStorage.removeItem('url');
 
-    //Cambiar ruta del div goBack para poder volver a la página correcta.
-let goBack=document.getElementById();
-let name = document.getElementById("name");
+let goBack = document.getElementById("divGoBack");
+let nameTxt = document.getElementById("name");
 let lista = document.getElementById("friendBody");
 
-getUserById(id).then(({username, friends}) =>{
-    name.innerHTML += username;
-    for(let i = 0; i<friends.length; i++){
-        getUserById(friends[i]).then(friend =>{
-            lista.innerHTML += ` 
-                        <div class="friendComponent d-flex flex-column justify-content-center align-items-center">
-                            <img class="imgFriend" src="/img/avatar.png" alt="">
-                            <p onclick="view(${friend.id})" class="dataProfileTxt">${friend.username}</p>
-                        </div> 
-                        `
-            ;
-        });
-    };
+
+goBack.innerHTML += `
+        <div id ="goBack" class="d-flex align-items-center">
+            <a onclick="back()">
+                <div class="d-flex align-items-center">
+                    <img src="/img/goBack.svg" alt="Volver">
+                    <h6>Volver</h6>
+                </div>
+            </a>
+        </div>
+        `;
+
+function back(){
+    window.history.back();
+}
+
+getCurrentUser().then((user)=>{
+    getUserById(id).then(({username, friends}) =>{
+        nameTxt.innerHTML += username;
+        for(let i = 0; i<friends.length; i++){
+            getUserById(friends[i]).then(friend =>{
+                if(friend.id == user.id){
+                    lista.innerHTML += ` 
+                            <div class="friendComponent d-flex flex-column justify-content-center align-items-center">
+                                <img class="imgFriend" src="/img/avatar.png" alt="">
+                                <a href="/pages/profilePage/profilePage.html" class="dataProfileTxt">${friend.username} (Yo)</a>
+                            </div> 
+                            `
+                            ;
+                } else{
+                    lista.innerHTML += ` 
+                            <div class="friendComponent d-flex flex-column justify-content-center align-items-center">
+                                <img class="imgFriend" src="/img/avatar.png" alt="">
+                                <p onclick="view(${friend.id})" class="dataProfileTxt">${friend.username}</p>
+                            </div> 
+                            `
+                            ;
+                }
+            });
+        };
+    });
 });
+
 
 function view(id){
     localStorage.setItem('id', id);
-    localStorage.setItem('url', "/pages/friendProfilePage/friendProfilePage.html");
     window.location.href = "/pages/friendProfilePage/friendProfilePage.html";
 }
