3efaaf95b280e947d85fe52bffed0856c9ced227
Alejandro Miranda <amiranda110500@gmail.com>
2021-12-20 10:52:59 +0000

    Cambiadas URLs AddFriend

--- pages/addFriend/addFriend.js
+++ pages/addFriend/addFriend.js
@@ -4,7 +4,7 @@ let user;
 let info = document.getElementById("info");
 getCurrentUser().then(data =>{
     if(data === undefined){
-        window.location.replace("http://localhost:5500/pages/index/index.html");
+        window.location.replace("/pages/index/index.html");
     } else{
         user = data
     }
