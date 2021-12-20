aa1e40cfc419792f49f1c50baa67ef9f4a7e05d7
Alejandro Miranda <amiranda110500@gmail.com>
2021-12-20 20:02:37 +0000

    Añadido texto informativo

--- pages/friendRequestPage/friendRequestPage.js
+++ pages/friendRequestPage/friendRequestPage.js
@@ -4,24 +4,28 @@ let userData;
 getCurrentUser().then(u => {
     userData = u;
     let friendRequest = u.friendRequest
-    for(let i = friendRequest.length-1; i>=0; i--){
-        let fr = friendRequest[i].request;
-        getUserById(fr.sender).then(user =>{
-            lista.innerHTML += 
-            `<div class="row d-flex align-items-center justify-content-center" id="row${i}">
-                <div id="contenedorfila" class="fila d-flex align-items-center justify-content-center  border-3 border border-dark bg-light g-gradient">
-                    <div class="d-flex align-items-center" id="friend-request"> 
-                        <h6 onclick="seeRequestProfile(${user.id})">${user.username}</h6>
-                        <div class="botones"> 
-                            <button type="button" onclick="acceptRequest(${fr.id},${user.id},row${i})" class="btn bg-blue"> Aceptar</button>
-                            <button type="button" onclick="declineRequest(${fr.id},${user.id},row${i})" class="btn btn-danger"> Rechazar </button>
+    if(friendRequest.length <= 0) {
+        lista.innerHTML = `<h3 style="color: azure;">No tienes ninguna solicitud pendiente</h3>`
+    } else {
+        for(let i = friendRequest.length-1; i>=0; i--){
+            let fr = friendRequest[i].request;
+            getUserById(fr.sender).then(user =>{
+                lista.innerHTML += 
+                `<div class="row d-flex align-items-center justify-content-center" id="row${i}">
+                    <div id="contenedorfila" class="fila d-flex align-items-center justify-content-center  border-3 border border-dark bg-light g-gradient">
+                        <div class="d-flex align-items-center" id="friend-request"> 
+                            <h6 onclick="seeRequestProfile(${user.id})">${user.username}</h6>
+                            <div class="botones"> 
+                                <button type="button" onclick="acceptRequest(${fr.id},${user.id},row${i})" class="btn bg-blue"> Aceptar</button>
+                                <button type="button" onclick="declineRequest(${fr.id},${user.id},row${i})" class="btn btn-danger"> Rechazar </button>
+                            </div>
                         </div>
+            
                     </div>
-        
                 </div>
-            </div>
-            `;            
-        }); 
+                `;            
+            }); 
+        }
     }
 });
 
